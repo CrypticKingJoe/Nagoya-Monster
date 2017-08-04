32c862181eae05efe5c0ddb44ccc732187dd1471
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-08-04 20:55:43 +0900

    Upgrade Log Class

--- Nagoya Monster/Log.cs
+++ Nagoya Monster/Log.cs
@@ -6,20 +6,27 @@ namespace Nagoya_Monster
 {
     public static class Log
     {
-        public static void Add(string Message)
+        public static void Add(string Message, string dateTime = "")
         {
             if (Program.isDebugging)
             {
-                try
+                if (dateTime == "")
+                    dateTime = DateTime.Now.ToString();
+
+                for (int i = 0; i < 4; ++i)
                 {
-                    TextWriter textWriter = new StreamWriter("log.txt", true);
-                    textWriter.WriteLine(string.Format("[{0}] {1}", DateTime.Now.ToString(), Message));
-                    textWriter.Close();
-                }
-                catch (Exception ex)
-                {
-                    Thread.Sleep(1000);
-                    Log.Add(string.Format("Error Saving Log: {0}", ex.Message));
+                    try
+                    {
+                        TextWriter textWriter = new StreamWriter("log.txt", true);
+                        textWriter.WriteLine(string.Format("[{0}] {1}", dateTime, Message));
+                        textWriter.Close();
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Thread.Sleep(1000);
+                        Add(string.Format("Error Saving Log: {0}", ex.Message));
+                    }
                 }
             }
         }
